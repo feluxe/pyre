0e22298673522e57936f973fee7a86c2ee7d7017
Felix Meyer-Wolters <felix@egoversum.com>
2017-09-26 01:20:22 +0200

    Proto #2

--- README.md
+++ README.md
@@ -1,7 +1,77 @@
 
 
 ```
-Inject Help Text here.
+ ╔══════════╗
+ ║   pyre   ║
+ ╚══════════╝
+
+Get 'Universal Regex Matches' from Python's regex engines.
+
+Usage:
+    pyre [options] [PATTERN] [INPUT]...
+    pyre -h | --help
+
+    [PATTERN]           A regular expression.
+    [INPUT]...          Can be file (default) or string. Reads from STDIN if
+                        left empty.
+Options:
+
+Input Options:
+
+
+      --str             Process.
+      --json-in         Set INPUT/STDIN to JSON encoded 'universal regex match
+                        obj'
+                        # TODO: Is this needed?
+      --encoding STR    Must be python compatible. (default: utf-8).
+                        List of encodings that can be used:
+                          https://docs.python.org/3/library/codecs.html#standard-encodings
+
+Output Options:
+
+        *Add reprint interface here.*
+
+      --json-out        Return 'universal regex match objects' JSON encoded.
+      --clines          Set --cb and --ca to count lines instead of chars.
+      --cb NUM          Define NUM of lines or chars for the 'before-context'.
+      --ca NUM          Define NUM of lines or chars for the 'after-context'.
+      --cba NUM         Define NUM of lines or chars for both --ca and --cb.
+
+Search Options:
+
+      --regex           Use Python's newer 'regex' engine. (default)
+      --re              Use Python's *old* 're' engine for your search.
+      --dotall          Make the '.' special character match any character at
+                        all, including a newline; without this flag, '.' will
+                        match anything except a newline.
+      --multiline       When specified, the pattern character '^' matches at the
+                        beginning of the string and at the beginning of each
+                        line (immediately following each newline); and the
+                        pattern character '$' matches at the end of the string
+                        and at the end of each line (immediately preceding each
+                        newline).
+      --ignorecase      Perform case-insensitive matching
+
+Misc Options:
+
+      --debug           Print even more info to the console.
+      --version         Show version.
+  -h, --help            Show (this) help screen.
+
+
+Escaping:
+
+    Use backslashes to escape Regex-Special-Chars. E.g.:
+      \.hello
+
+Examples:
+
+    #TODO: This
+
+
+
+
+
 ```
 
 # pyre
--- pyre/api/search.py
+++ pyre/api/search.py
@@ -1,12 +1,14 @@
-import regex
+# import re
+import regex as re
 import sys
-import re
+import os
 import io
 import time
 import mmap
 import json
 from typing import Optional, List, Pattern, NamedTuple, Union, Sequence
 import pyre.constants as const
+from pyre.utils import eprint
 from multiprocessing import Pool
 
 
@@ -129,14 +131,11 @@ def _process_file(
 ) -> None:
     """
     """
-    try:
-        source: mmap.mmap = mmap.mmap(
-            source_file_obj.fileno(),
-            0,
-            access=mmap.ACCESS_READ,
-        )
-    except ValueError:
-        return
+    source: mmap.mmap = mmap.mmap(
+        source_file_obj.fileno(),
+        0,
+        access=mmap.ACCESS_READ,
+    )
 
     matches: Sequence[Match] = options.search_bytes.finditer(source)
 
@@ -154,7 +153,8 @@ def _process_file(
             matches=urm_matches,
         )
 
-        print(json.dumps(urm._asdict(), sort_keys=True, indent=4))
+        # print(json.dumps(urm._asdict(), sort_keys=True, indent=4))
+        print(file_path)
 
 
 def _try_process_file_input(
@@ -165,6 +165,12 @@ def _try_process_file_input(
     Validate current file and try to read it to search for matches and run
     substitutions.
     """
+    if os.path.isdir(file_path):
+        return
+
+    if os.path.getsize(file_path) > 2147483647:
+        eprint("Can't handle files larger than 2147483647 bytes")
+
     try:
         with open(file_path, 'r', encoding=options.encoding) as source_file_obj:
             _process_file(
@@ -176,13 +182,13 @@ def _try_process_file_input(
         return
 
     except PermissionError:
-        const.DEBUG and print(
+        const.DEBUG and eprint(
             'SKIP (Permission): {}'.format(file_path)
         )
         return
 
     except UnicodeDecodeError:
-        const.DEBUG and print(
+        const.DEBUG and eprint(
             'SKIP (UnicodeDecode): {}'.format(file_path)
 
         )
@@ -214,6 +220,7 @@ def _process_chunk(args):
     if options.file_input:
         for line in args[0]:
             _try_process_file_input(line, options)
+            # _try_process_file_input(args[0], options)
     elif options.string_input:
         whole = ''
         for line in args[0]:
@@ -234,12 +241,16 @@ def _chunk_input(
     items: []
 ):
     chunk = []
+    max = 4
+    i = 0
     for item in items:
+        if i < max:
+            i += 1
         chunk.append(item)
 
-        if len(chunk) > 3:
-            yield chunk[0:3]
-            chunk = chunk[3:]
+        if len(chunk) > i:
+            yield chunk[0:i]
+            chunk = chunk[i:]
 
     yield chunk
 
@@ -265,7 +276,8 @@ def search(
 
     flag_dotall = re.DOTALL if dotall else 0
     flag_ignorecase = re.IGNORECASE if ignorecase else 0
-    flags = flag_dotall | flag_ignorecase
+    flag_v = re.V0
+    flags = flag_v | flag_dotall | flag_ignorecase
 
     search_bytes: Pattern = re.compile(bytes(search, encoding), flags)
     search_expr: Pattern = re.compile(search, flags)
@@ -288,7 +300,7 @@ def search(
 
     def run_async_pool():
         with Pool() as p:
-            p.map(_process_chunk, _get_pool_args(), chunksize=5)
+            p.map(_process_chunk, _get_pool_args(), chunksize=1)
 
     def run_sync():
         _process_chunk((
@@ -300,8 +312,8 @@ def search(
         run_sync()
 
     else:
-        run_sync()
-        # run_async_pool()
+        # run_sync()
+        run_async_pool()
 
         # for line in input_data:
         #     print(line)
@@ -321,4 +333,4 @@ def search(
     # if const.DEBUG:
     end_time = time.time()
     elapsed = end_time - start_time
-    print(elapsed)
+    print('source":', elapsed)
